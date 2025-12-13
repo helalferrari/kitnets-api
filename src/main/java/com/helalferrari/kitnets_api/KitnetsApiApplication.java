50199d556249178fd0d5566d66e1397551541181
Helal Ferrari <ferrari.work@gmail.com>
2025-12-13 18:04:27 -0300

    Inicialização da estrutura do projeto com spring initializr

---
+++ src/main/java/com/helalferrari/kitnets_api/KitnetsApiApplication.java
@@ -0,0 +1,13 @@
+package com.helalferrari.kitnets_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitnetsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitnetsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helalferrari/kitnets_api/KitnetsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helalferrari.kitnets_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitnetsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
